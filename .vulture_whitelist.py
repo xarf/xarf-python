a5d9588a830364b63047f242b38f9c9128da4204
Victor Lopez <victor@thevictorlopez.com>
2026-03-24 13:43:35 +0100

    Cleanup base python project.

--- .vulture_whitelist.py
+++
@@ -1,5 +0,0 @@
-# Vulture whitelist for intentionally unused code
-# https://github.com/jendrikseipp/vulture
-
-# Pydantic validators require 'cls' parameter even if unused
-_.cls  # unused variable (validators)
