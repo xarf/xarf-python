239df049885f9f1dbe0d323aea8f7a0d6e0b7a31
Victor Lopez <victor@thevictorlopez.com>
2026-03-24 18:02:02 +0100

    Add types for all report Category and Type combinations

--- xarf/__init__.py
+++ xarf/__init__.py
@@ -1,27 +1,217 @@
-"""XARF v4 Python Parser.
+"""XARF v4 Python library.
 
-A Python library for parsing and validating XARF v4
-(eXtended Abuse Reporting Format) reports.
-Includes backwards compatibility with XARF v3.
+A Python library for parsing, generating, and validating XARF v4
+(eXtended Abuse Reporting Format) reports. Includes backwards
+compatibility with XARF v3.
+
+Example:
+    >>> from xarf import parse, create_report, create_evidence
+    >>> result = parse(json_data)
+    >>> result.report
+    SpamReport(...)
 """
 
-__version__ = "4.0.0a1"
+from xarf.exceptions import (
+    XARFError,
+    XARFParseError,
+    XARFSchemaError,
+    XARFValidationError,
+)
+from xarf.models import (
+    AnyXARFReport,
+    ContactInfo,
+    CreateReportResult,
+    ParseResult,
+    ValidationError,
+    ValidationWarning,
+    XARFEvidence,
+    XARFReport,
+)
+from xarf.types_connection import (
+    ConnectionBaseReport,
+    ConnectionReport,
+    DdosReport,
+    InfectedHostReport,
+    LoginAttackReport,
+    PortScanReport,
+    ReconnaissanceReport,
+    ScrapingReport,
+    SqlInjectionReport,
+    VulnerabilityScanReport,
+)
+from xarf.types_content import (
+    BrandInfringementReport,
+    CompromiseIndicator,
+    ContentBaseReport,
+    ContentReport,
+    CsamReport,
+    CsemReport,
+    ExposedDataReport,
+    FraudReport,
+    MalwareReport,
+    PhishingReport,
+    RegistrantDetails,
+    RemoteCompromiseReport,
+    SuspiciousRegistrationReport,
+    WebshellDetails,
+)
+from xarf.types_copyright import (
+    CopyrightBaseReport,
+    CopyrightCopyrightReport,
+    CopyrightCyberlockerReport,
+    CopyrightLinkSiteReport,
+    CopyrightP2pReport,
+    CopyrightReport,
+    CopyrightUgcPlatformReport,
+    CopyrightUsenetReport,
+    CyberlockerTakedownInfo,
+    CyberlockerUploaderInfo,
+    FileInfo,
+    LinkedContentItem,
+    LinkSiteLinkInfo,
+    LinkSiteRanking,
+    MessageInfo,
+    PeerInfo,
+    SwarmInfo,
+    UgcContentInfo,
+    UgcMatchDetails,
+    UgcMonetizationInfo,
+    UgcUploaderInfo,
+    UsenetEncodingInfo,
+    UsenetNzbInfo,
+    UsenetServerInfo,
+)
+from xarf.types_infrastructure import (
+    BotnetReport,
+    CompromisedServerReport,
+    InfrastructureBaseReport,
+    InfrastructureReport,
+)
+from xarf.types_messaging import (
+    BulkIndicators,
+    BulkMessagingReport,
+    MessagingBaseReport,
+    MessagingReport,
+    SpamIndicators,
+    SpamReport,
+)
+from xarf.types_reputation import (
+    BlocklistReport,
+    ReputationBaseReport,
+    ReputationReport,
+    ThreatIntelligenceReport,
+)
+from xarf.types_vulnerability import (
+    CveReport,
+    ImpactAssessment,
+    MisconfigurationReport,
+    OpenServiceReport,
+    VulnerabilityBaseReport,
+    VulnerabilityReport,
+)
+from xarf.v3_compat import convert_v3_to_v4, is_v3_report
+
+__version__ = "0.1.0.dev0"
 __author__ = "XARF Project"
 __email__ = "contact@xarf.org"
 
-from .exceptions import XARFError, XARFParseError, XARFValidationError
-from .generator import XARFGenerator
-from .models import XARFReport
-from .parser import XARFParser
-from .v3_compat import convert_v3_to_v4, is_v3_report
+# Spec version this library was built against.
+SPEC_VERSION = "4.2.0"
 
 __all__ = [
-    "XARFParser",
+    # Version
+    "SPEC_VERSION",
+    # Result types
+    "AnyXARFReport",
+    "ParseResult",
+    "CreateReportResult",
+    "ValidationError",
+    "ValidationWarning",
+    # Base models
     "XARFReport",
+    "XARFEvidence",
+    "ContactInfo",
+    # Exceptions
     "XARFError",
     "XARFValidationError",
     "XARFParseError",
-    "XARFGenerator",
-    "convert_v3_to_v4",
+    "XARFSchemaError",
+    # v3 compatibility
     "is_v3_report",
+    "convert_v3_to_v4",
+    # Messaging
+    "MessagingBaseReport",
+    "SpamIndicators",
+    "SpamReport",
+    "BulkIndicators",
+    "BulkMessagingReport",
+    "MessagingReport",
+    # Connection
+    "ConnectionBaseReport",
+    "LoginAttackReport",
+    "PortScanReport",
+    "DdosReport",
+    "InfectedHostReport",
+    "ReconnaissanceReport",
+    "ScrapingReport",
+    "SqlInjectionReport",
+    "VulnerabilityScanReport",
+    "ConnectionReport",
+    # Content
+    "ContentBaseReport",
+    "PhishingReport",
+    "MalwareReport",
+    "CsamReport",
+    "CsemReport",
+    "ExposedDataReport",
+    "BrandInfringementReport",
+    "FraudReport",
+    "CompromiseIndicator",
+    "WebshellDetails",
+    "RemoteCompromiseReport",
+    "RegistrantDetails",
+    "SuspiciousRegistrationReport",
+    "ContentReport",
+    # Infrastructure
+    "InfrastructureBaseReport",
+    "BotnetReport",
+    "CompromisedServerReport",
+    "InfrastructureReport",
+    # Copyright
+    "CopyrightBaseReport",
+    "CopyrightCopyrightReport",
+    "SwarmInfo",
+    "PeerInfo",
+    "CopyrightP2pReport",
+    "FileInfo",
+    "CyberlockerTakedownInfo",
+    "CyberlockerUploaderInfo",
+    "CopyrightCyberlockerReport",
+    "UgcContentInfo",
+    "UgcUploaderInfo",
+    "UgcMatchDetails",
+    "UgcMonetizationInfo",
+    "CopyrightUgcPlatformReport",
+    "LinkSiteLinkInfo",
+    "LinkedContentItem",
+    "LinkSiteRanking",
+    "CopyrightLinkSiteReport",
+    "MessageInfo",
+    "UsenetEncodingInfo",
+    "UsenetNzbInfo",
+    "UsenetServerInfo",
+    "CopyrightUsenetReport",
+    "CopyrightReport",
+    # Vulnerability
+    "VulnerabilityBaseReport",
+    "ImpactAssessment",
+    "CveReport",
+    "OpenServiceReport",
+    "MisconfigurationReport",
+    "VulnerabilityReport",
+    # Reputation
+    "ReputationBaseReport",
+    "BlocklistReport",
+    "ThreatIntelligenceReport",
+    "ReputationReport",
 ]
